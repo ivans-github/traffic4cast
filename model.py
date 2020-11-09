26b332bc035eb94d7973d9a5ab61531df99b9c9d
ivans-github <55021678+ivans-github@users.noreply.github.com>
2020-11-09 16:32:47 +0000

    Add files via upload

---
+++ model.py
@@ -0,0 +1,120 @@
+from fastai.basics import *
+
+############################################################################################
+
+def ConvLayer(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    '''Conv2d + ELU'''
+    return nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+                         nn.ELU(inplace=True))
+
+def ConvBlock(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    '''Conv2d + ELU + GroupNorm'''
+    return nn.Sequential(ConvLayer(in_channels, out_channels, kernel_size, stride, padding),
+                         nn.GroupNorm(num_groups=8, num_channels=out_channels, eps=1e-6))
+
+############################################################################################
+
+class DenseBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, nb_layers=4):
+        super().__init__()
+        self.first_layer = ConvBlock(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.mid_layers = nn.ModuleList([ConvBlock(in_channels + i*out_channels, out_channels, kernel_size=3, stride=1, padding=1) 
+                                         for i in range(1, nb_layers)])
+        self.last_layer = ConvLayer(in_channels + nb_layers*out_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        
+    def forward(self, x):
+        layers_concat = list()
+        layers_concat.append(x)
+        layers_concat.append(self.first_layer(x))
+        
+        for mid_layer in self.mid_layers:
+            layers_concat.append(mid_layer(torch.cat(layers_concat, dim=1)))
+            
+        return self.last_layer(torch.cat(layers_concat, dim=1))
+
+def AvgPoolDenseBlock(in_channels, out_channels, nb_layers=4):
+    return nn.Sequential(nn.AvgPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True),
+                         DenseBlock(in_channels, out_channels, nb_layers))
+
+class UpConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.convtrans = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
+        self.elu = nn.ELU(inplace=True)
+    
+    def forward(self, x, x_enc):
+        x = self.convtrans(x, output_size=x_enc.shape[-2:])
+        x = self.elu(x)
+        return torch.cat([x, x_enc], dim=1)
+
+############################################################################################
+
+class NetO(nn.Module):
+    def __init__(self, num_in_frames=12, num_out_frames=12):
+        super().__init__()
+        
+        self.enc1 = DenseBlock(9*num_in_frames+3*9+7+1, 64, 4)
+        self.enc2 = AvgPoolDenseBlock(64, 96, 4)
+        self.enc3 = AvgPoolDenseBlock(96, 128, 4)
+        self.enc4 = AvgPoolDenseBlock(128, 128, 4)
+        self.enc5 = AvgPoolDenseBlock(128, 128, 4)
+        self.enc6 = AvgPoolDenseBlock(128, 128, 4)
+        self.enc7 = AvgPoolDenseBlock(128, 128, 4)
+        self.enc8 = AvgPoolDenseBlock(128, 128, 4)
+        
+        self.bridge = ConvBlock(128, 128)
+        
+        self.dec7_1 = UpConvBlock(128, 128)
+        self.dec7_2 = ConvBlock(128+128, 128)
+        self.dec6_1 = UpConvBlock(128, 128)
+        self.dec6_2 = ConvBlock(128+128, 128)
+        self.dec5_1 = UpConvBlock(128, 128)
+        self.dec5_2 = ConvBlock(128+128, 128)
+        self.dec4_1 = UpConvBlock(128, 128)
+        self.dec4_2 = ConvBlock(128+128, 128)
+        self.dec3_1 = UpConvBlock(128, 128)
+        self.dec3_2 = ConvBlock(128+128, 128)
+        self.dec2_1 = UpConvBlock(128, 128)
+        self.dec2_2 = ConvBlock(128+96, 128)
+        self.dec1_1 = UpConvBlock(128, 128)
+        self.dec1_2 = ConvBlock(128+64, 128)
+        
+        self.out_1 = nn.Conv2d(128, 8*num_out_frames, kernel_size=3, stride=1, padding=1)
+        self.out_2 = nn.Sigmoid()
+          
+    def forward(self, x):
+        
+        x0 = x[:,:108,...]               # 4, 108, 495, 436
+        s = x[:,108:115,...]             # 4, 7, 495, 436
+        t = x[:,115,...].unsqueeze(1)    # 4, 1, 495, 436
+        
+        N, C, H, W = x0.shape            # 4, 108, 495, 436
+        x0r = x0.reshape(N, 9, 12, H, W) # 4, 9, 12, 495, 436
+        x0_mean = x0r.mean(dim=2)        # 4, 9, 495, 436
+        x0_std = x0r.std(dim=2)
+        x0_rng = x0r.max(dim=2).values - x0r.min(dim=2).values
+        
+        x = torch.cat([x0, x0_mean, x0_std, x0_rng, s, t], dim=1)
+        x = x / 255.
+        
+        x1 = self.enc1(x)
+        x2 = self.enc2(x1)
+        x3 = self.enc3(x2)
+        x4 = self.enc4(x3)
+        x5 = self.enc5(x4)
+        x6 = self.enc6(x5)
+        x7 = self.enc7(x6)
+        x8 = self.enc8(x7)
+        
+        x100 = self.bridge(x8)
+        
+        x107 = self.dec7_2(self.dec7_1(x100, x7))
+        x106 = self.dec6_2(self.dec6_1(x107, x6))
+        x105 = self.dec5_2(self.dec5_1(x106, x5))
+        x104 = self.dec4_2(self.dec4_1(x105, x4))
+        x103 = self.dec3_2(self.dec3_1(x104, x3))
+        x102 = self.dec2_2(self.dec2_1(x103, x2))
+        x101 = self.dec1_2(self.dec1_1(x102, x1))
+        
+        out = self.out_2(self.out_1(x101))*255.
+        return out.view(N, 8, -1, H, W)
---
+++ traffic_dataset.py
@@ -0,0 +1,89 @@
+import h5py
+import tarfile
+from fastai.basics import *
+
+############################################################################################
+
+def untar(filename, outpath='../storage/'):
+    tarred_file = tarfile.open(filename)
+    tarred_file.extractall(path=outpath)
+    tarred_file.close()
+    return Path('{}{}/'.format(outpath,
+                               os.path.basename(filename).split('.')[0]))
+
+############################################################################################
+
+class TrafficDataset(torch.utils.data.Dataset):
+    
+    def __init__(self, city='MOSCOW', partition='training', num_in_frames=12, num_out_frames=12, time_step=1, bs=None):
+        
+        self.path = Path(f'../storage/{city}/{partition}')
+        self.num_in_frames = num_in_frames
+        self.num_out_frames = num_out_frames
+        self.time_step = time_step
+        self.bs = bs
+        self.num_frames_perday = 1 + (288 - self.num_in_frames - self.num_out_frames)
+        
+        if not self.path.exists():
+            untar(f'../storage/{city}.tar')
+        
+        self.num_days = len(self.path.ls())
+        self.filelist = sorted(self.path.ls())
+        
+        # load static features
+        with h5py.File(f'../storage/{city}/{city}_static_2019.h5', 'r') as static_file:
+            static_features = static_file.get('array')[()].astype(np.float32)
+            static_features = torch.from_numpy(static_features).permute(2, 0, 1)
+        self.static_features = static_features
+        
+        # is_empty and n_inp are required for fastai to work
+        self.is_empty = False if len(self)>0 else True
+        self.n_inp = 1
+    
+    def __len__(self):
+        if self.bs == None:
+            return math.ceil((self.num_days * self.num_frames_perday) / self.time_step)
+        else:
+            return math.ceil((self.num_days * self.num_frames_perday) / self.time_step) // self.bs
+    
+    def __getitem__(self, batch_indices):
+        
+        if not isinstance(batch_indices, Iterable): batch_indices = [batch_indices]
+        
+        if (self.bs != None) and (len(batch_indices)==1): 
+            batch_indices = range(batch_indices[0]*self.bs, (batch_indices[0]+1)*self.bs)
+        
+        # Add time_step to batch_indices
+        batch_indices = [index * self.time_step for index in batch_indices]
+        
+        indices = collections.defaultdict(list)
+        for index in batch_indices:
+            indices[index // self.num_frames_perday].append(index % self.num_frames_perday)
+        
+        batch = []
+        for file_idx, frame_idxs in indices.items():
+            
+            raw = h5py.File(self.filelist[file_idx], 'r').get('array')
+            extract = raw[frame_idxs[0]:(frame_idxs[-1] + self.num_in_frames + self.num_out_frames), ...].astype(np.float32)
+            
+            for frame_idx in frame_idxs:
+                i = frame_idx - frame_idxs[0]
+                
+                x = torch.from_numpy(extract[i:i + self.num_in_frames,...]).permute(3, 0, 1, 2)
+                C, D, H, W = x.shape
+                x = x.reshape(C*D, H, W)
+                t = torch.ones(1, H, W) * frame_idx * 255. / (288. - self.num_in_frames) # add time feature (t) - scaled to 255
+                x = torch.cat([x, self.static_features, t], dim=0)
+                
+                y = torch.from_numpy(extract[i + self.num_in_frames:i + self.num_in_frames + self.num_out_frames,
+                                             ...]).permute(3, 0, 1, 2)
+                y = y[:8, ...]
+                
+                batch.append((x, y))
+
+        if len(batch) == 1:
+            return torch.from_numpy(batch[0][0]), torch.from_numpy(batch[0][1])
+        elif self.bs != None:
+            return torch.stack([sample[0] for sample in batch]), torch.stack([sample[1] for sample in batch])
+        else:
+            return batch
